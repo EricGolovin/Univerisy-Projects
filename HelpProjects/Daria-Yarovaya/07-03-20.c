4406d6db842ab84c891e8850f13ec44ea6928200
EricGolovin <ericgolovin@outlook.com>
2020-04-07 21:58:41 +0300

    added 1 help-work

---
+++ HelpProjects/Daria-Yarovaya/07-03-20.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main(void) {
+    int m, n;
+    
+    printf("Enter M: ");
+    scanf("%d", &m);
+    printf("Enter N: ");
+    scanf("%d", &n);
+    printf("\n");
+    
+    int array[m][n];
+    
+    for (int x = 0; x < m; x++) {
+        for (int y = 0; y < n; y++) {
+            printf("Enter value for [%d][%d]: ", x, y);
+            scanf("%d", &array[x][y]);
+        }
+    }
+    
+    int isNegative, changeCounter = 0;
+    
+    if (array[0][0] < 0) {
+        isNegative = 1;
+    } else {
+        isNegative = 0;
+    }
+    
+    for (int x = 0; x < m; x++) {
+        for (int y = 0; y < n; y++) {
+            if (array[x][y] > 0 && isNegative == 0) {
+                isNegative = 1;
+                changeCounter++;
+            } else if (array[x][y] < 0 && isNegative == 1) {
+                isNegative = 1;
+                changeCounter++;
+            }
+        }
+    }
+    
+    for (int x = 0; x < m; x++) {
+        for (int y = 0; y < n; y++) {
+            printf("%d", array[x][y]);
+        }
+        printf("\n");
+    }
+    
+    printf("Total changes: %d\n", changeCounter);
+    return 0;
+}
