23d36c257a55c37ebed187f2dce9f3f3944b1e3b
Eric Golovin <ericgolovin@outlook.com>
2020-11-25 00:36:05 +0200

    add mary hw

---
+++ help-projects/Mary/DotNetLab04-Mary/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace DotNetLab04_Mary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 221);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(59, 24);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.Text = "Toy";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 251);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(71, 24);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Book";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(12, 281);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(154, 24);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.Text = "Sport Equipment";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Checked = true;
+            this.radioButton4.Location = new System.Drawing.Point(12, 311);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(49, 24);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "all";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 341);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(199, 97);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(199, 118);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Generate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(217, 13);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox1.Size = new System.Drawing.Size(571, 425);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ help-projects/Mary/DotNetLab04-Mary/Form1.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DotNetLab04_Mary
+{
+    public partial class Form1 : Form
+    {
+
+        List<object> listOfProducts = new List<object>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                radioButton2.Checked = false;
+                radioButton3.Checked = false;
+                radioButton4.Checked = false;
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked == true)
+            {
+                radioButton1.Checked = false;
+                radioButton3.Checked = false;
+                radioButton4.Checked = false;
+            }
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton3.Checked == true)
+            {
+                radioButton1.Checked = false;
+                radioButton2.Checked = false;
+                radioButton4.Checked = false;
+            }
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton4.Checked == true)
+            {
+                radioButton1.Checked = false;
+                radioButton2.Checked = false;
+                radioButton3.Checked = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            string textBoxText = "";
+            if (radioButton1.Checked == true)
+            {
+                foreach (object product in listOfProducts)
+                {
+                    if (product is Toy)
+                    {
+                        Toy toyProduct = product as Toy;
+                        textBoxText += toyProduct.getDescription();
+                        textBoxText += "\r\n---------------------\r\n";
+                    }
+                }
+            } else if (radioButton2.Checked == true)
+            {
+                foreach (object product in listOfProducts)
+                {
+                    if (product is Book)
+                    {
+                        if (product is Book)
+                        {
+                            Book bookProduct = product as Book;
+                            textBoxText += bookProduct.getDescription();
+                            textBoxText += "\r\n---------------------\r\n";
+                        }
+                    }
+                }
+            } else if (radioButton3.Checked == true)
+            {
+                foreach (object product in listOfProducts)
+                {
+                    if (product is SportEquipment)
+                    {
+                        if (product is SportEquipment)
+                        {
+                            SportEquipment sportEquipmentProduct = product as SportEquipment;
+                            textBoxText += sportEquipmentProduct.getDescription();
+                            textBoxText += ("\r\n---------------------\r\n");
+                        }
+                    }
+                }
+            } else if (radioButton4.Checked == true)
+            {
+                foreach (object product in listOfProducts)
+                {
+                    if (product is Toy)
+                    {
+                        Toy toyProduct = product as Toy;
+                        textBoxText += toyProduct.getDescription();
+                        textBoxText += "\r\n---------------------\r\n";
+                    }
+                    if (product is Book)
+                    {
+                        Book bookProduct = product as Book;
+                        textBoxText += bookProduct.getDescription();
+                        textBoxText += "\r\n---------------------\r\n";
+                    }
+                    if (product is SportEquipment)
+                    {
+                        SportEquipment sportEquipmentProduct = product as SportEquipment;
+                        textBoxText += sportEquipmentProduct.getDescription();
+                        textBoxText += "\r\n---------------------\r\n";
+                    }
+                }
+                
+            }
+
+            textBox1.Text = textBoxText;
+
+            if (textBox1.Text == "" && listOfProducts.Count != 0)
+            {
+                textBox1.Text = "Nothing Found";
+            } else if (textBox1.Text == "")
+            {
+                textBox1.Text = "No items in the list";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            Random rnd = new Random();
+            List<object> myList = new List<object>();
+            for (int i = 0; i < 10; i++)
+            {
+                int newRandomPrice = rnd.Next(10, 100);
+                int newRandomAge = rnd.Next(2, 8);
+                Toy newToy = new Toy(newRandomPrice, $"toy{i + 1}", newRandomAge, $"{i * newRandomAge}-Corporation", $"some-{i + 2}");
+                myList.Add(newToy);
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                int newRandomPrice = rnd.Next(10, 100);
+                int newRandomAge = rnd.Next(2, 8);
+                Book newBook = new Book(newRandomPrice, $"toy{i + 1}", newRandomAge, $"{i * newRandomAge}-Corporation", $"Joh-{i + 2}");
+                myList.Add(newBook);
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                int newRandomPrice = rnd.Next(10, 100);
+                int newRandomAge = rnd.Next(2, 8);
+                SportEquipment newToy = new SportEquipment(newRandomPrice, $"toy{i + 1}", newRandomAge, $"{i * newRandomAge}-Corporation");
+                myList.Add(newToy);
+            }
+            listOfProducts = myList.OrderBy(x => Guid.NewGuid()).ToList();
+
+            string textBoxText = "";
+            foreach (object product in listOfProducts)
+            {
+                if (product is Toy)
+                {
+                    Toy toyProduct = product as Toy;
+                    textBoxText += toyProduct.getDescription();
+                    textBoxText += "\r\n---------------------\r\n";
+                }
+                if (product is Book)
+                {
+                    Book bookProduct = product as Book;
+                    textBoxText += bookProduct.getDescription();
+                    textBoxText += "\r\n---------------------\r\n";
+                }
+                if (product is SportEquipment)
+                {
+                    SportEquipment sportEquipmentProduct = product as SportEquipment;
+                    textBoxText += sportEquipmentProduct.getDescription();
+                    textBoxText += "\r\n---------------------\r\n";
+                }
+            }
+            textBox1.Text = textBoxText;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    public class Product
+    {
+        public double price;
+        public string name;
+        public float allowedAgeToUseFrom;
+        public Product(double price, string name, float allowedAgeToUseFrom)
+        {
+            this.price = price;
+            this.name = name;
+            this.allowedAgeToUseFrom = allowedAgeToUseFrom;
+        }
+
+        public virtual string getDescription()
+        {
+            return "Product";
+        }
+    }
+
+    public class Toy : Product
+    {
+        string manufacturer;
+        string material;
+        public Toy(double price, string name, float allowedAgeToUseFrom, string manufacturer, string material) : base(price, name, allowedAgeToUseFrom)
+        {
+            this.manufacturer = manufacturer;
+            this.material = material;
+        }
+
+        public override string getDescription()
+        {
+            return $"Toy - Name: {this.name}, price: {this.price}, Allowed age to use from: {this.allowedAgeToUseFrom}, Manufacturer: {this.manufacturer}, Material: {this.material}";
+        }
+    }
+
+    public class Book : Product
+    {
+        string author;
+        string publisher;
+        public Book(double price, string name, float allowedAgeToUseFrom, string author, string publisher) : base(price, name, allowedAgeToUseFrom)
+        {
+            this.author = author;
+            this.publisher = publisher;
+        }
+
+        public override string getDescription()
+        {
+            return $"Book - Name: {this.name}, price: {this.price}, Allowed age to use from: {this.allowedAgeToUseFrom}, Author: {this.author}, Publisher: {this.publisher}";
+        }
+    }
+    
+    public class SportEquipment : Product
+    {
+        string manufacturer;
+        public SportEquipment(double price, string name, float allowedAgeToUseFrom, string manufacturer) : base(price, name, allowedAgeToUseFrom)
+        {
+            this.manufacturer = manufacturer;
+        }
+
+        public override string getDescription()
+        {
+            return $"Sport Equipment - Name: {this.name}, price: {this.price}, Allowed age to use from: {this.allowedAgeToUseFrom}, Manufacturer: {this.manufacturer}";
+        }
+    }
+}
