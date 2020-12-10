d8f495a28471bd2c44382cd5dbce332efed05645
Eric Golovin <ericgolovin@outlook.com>
2020-12-10 23:58:59 +0200

    Add Shared Network. Add ModelProxy file.

---
+++ help-projects/Mary/CourseWork-Atelie/CourseWork-Atelie/Networking/Shared/NetworkRequestInterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseWork_Atelie.Networking.Shared
+{
+    interface NetworkRequest<out T>
+    {
+        T Get(string request);
+        void Add(string request);
+    }
+}
---
+++ help-projects/Mary/CourseWork-Atelie/CourseWork-Atelie/Networking/Shared/SQLDatabaseConnetion.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace CourseWork_Atelie.Networking.Shared
+{
+    class SQLDatabaseConnetion
+    {
+        public static readonly SQLDatabaseConnetion instance = new SQLDatabaseConnetion();
+
+        private SQLDatabaseConnetion() { }
+
+        private static string databaseConnectionConst = @"DataSource=.\SQLEXPRESS;InitialCatalog=Atelie;IntegratedSecurity=True";
+        private SqlConnection connection = new SqlConnection(databaseConnectionConst);
+
+        public SqlDataReader Get(string command)
+        {
+            connection.Open();
+            Console.WriteLine("Connection opened");
+            SqlCommand newCommand = new SqlCommand(command, connection);
+            SqlDataReader reader = newCommand.ExecuteReader();
+            connection.Close();
+            Console.WriteLine("Connection closed");
+            return reader;
+        }
+
+        public void Insert(string command)
+        {
+            int counter;
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Connection opened");
+
+                SqlCommand newCommand = new SqlCommand(command, connection);
+
+                counter = newCommand.ExecuteNonQuery();
+                Console.WriteLine($"Number of objects inserted: {counter}");
+
+                connection.Close();
+                Console.WriteLine("Connection closed");
+            }
+            catch (SqlException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+}
