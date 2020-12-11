17b03990dd40f7c07a4cd7b51033f2d8e60c2310
Eric Golovin <ericgolovin@outlook.com>
2020-12-12 01:48:21 +0200

    Add SingleDependency Network Proxies

---
+++ help-projects/Mary/CourseWork-Atelie/CourseWork-Atelie/Networking/ClientNetworkProxy.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace CourseWork_Atelie.Networking
+{
+    class ClientNetworkProxy : Shared.NetworkRequest<List<Client>>
+    {
+        private readonly Shared.SQLDatabaseConnetion connection = Shared.SQLDatabaseConnetion.instance;
+        public List<Client> Get(string request)
+        {
+            List<Client> resultList = new List<Client>();
+            try
+            {
+                SqlDataReader reader = connection.Get(request);
+                while (reader.Read())
+                {
+                    int id = Convert.ToInt32(reader.GetValue(0));
+                    string fullName = Convert.ToString(reader.GetValue(1));
+                    string phoneNumber = Convert.ToString(reader.GetValue(2));
+                    string email = Convert.ToString(reader.GetValue(3));
+
+                    Client newObject = new Client(id, fullName, phoneNumber, email);
+                    resultList.Add(newObject);
+                }
+            }
+            catch (SqlException exception)
+            {
+                Console.WriteLine(exception.Message);
+                Console.WriteLine("ModelList is Empty");
+            }
+            return resultList;
+        }
+        public void Add(string request)
+        {
+            connection.Insert(request);
+        }
+    }
+    class Client
+    {
+        public int id;
+        public string fullName;
+        public string phoneNumber;
+        public string email;
+
+        public Client(int id, string fullName, string phoneNumber, string email)
+        {
+            this.id = id;
+            this.fullName = fullName;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+        }
+    }
+}
---
+++ help-projects/Mary/CourseWork-Atelie/CourseWork-Atelie/Networking/CutterNetworkProxy.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace CourseWork_Atelie.Networking
+{
+    class CutterNetworkProxy : Shared.NetworkRequest<List<Cutter>>
+    {
+        private readonly Shared.SQLDatabaseConnetion connection = Shared.SQLDatabaseConnetion.instance;
+        public List<Cutter> Get(string request)
+        {
+            List<Cutter> resultList = new List<Cutter>();
+            try
+            {
+                SqlDataReader reader = connection.Get(request);
+                while (reader.Read())
+                {
+                    int id = Convert.ToInt32(reader.GetValue(0));
+                    string fullName = Convert.ToString(reader.GetValue(1));
+                    double salary = Convert.ToDouble(reader.GetValue(2));
+                    int numberOfOrders = Convert.ToInt32(reader.GetValue(3));
+
+                    Cutter newObject = new Cutter(id, fullName, salary, numberOfOrders);
+                    resultList.Add(newObject);
+                }
+            }
+            catch (SqlException exception)
+            {
+                Console.WriteLine(exception.Message);
+                Console.WriteLine("ModelList is Empty");
+            }
+            return resultList;
+        }
+        public void Add(string request)
+        {
+            connection.Insert(request);
+        }
+    }
+    class Cutter
+    {
+        public int id;
+        public string fullName;
+        public double salary;
+        public int numberOfOrders;
+
+        public Cutter(int id, string fullName, double salary, int numberOfOrders)
+        {
+            this.id = id;
+            this.fullName = fullName;
+            this.salary = salary;
+            this.numberOfOrders = numberOfOrders;
+        }
+    }
+}
