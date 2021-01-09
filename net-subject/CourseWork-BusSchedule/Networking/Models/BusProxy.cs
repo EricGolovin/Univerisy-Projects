b66f3e01f72beaa5d757016e8414d17aeb17c7ae
Eric Golovin <ericgolovin@outlook.com>
2021-01-09 23:25:52 +0200

    Add independent objects

---
+++ net-subject/CourseWork-BusSchedule/Networking/Models/BusProxy.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace CourseWork_BusSchedule.Networking.Models
+{
+    public class BusProxy
+    {
+        public static List<Bus> Get(string request)
+        {
+            List<Bus> resultList = new List<Bus>();
+            try
+            {
+                SqlDataReader reader = DatabaseConnection.shared.Get(request);
+                while (reader.Read())
+                {
+                    int id = Convert.ToInt32(reader.GetValue(0));
+                    string brand = Convert.ToString(reader.GetValue(1));
+                    int subId = Convert.ToInt32(reader.GetValue(2));
+                    double totalDistanceDriven = Convert.ToDouble(reader.GetValue(3));
+                    string releaseDateString = Convert.ToString(reader.GetValue(4));
+
+                    DateTime releaseDate = DateTime.Parse(releaseDateString);
+
+                    Bus newBus = new Bus(id, brand, subId, totalDistanceDriven, releaseDate);
+                    resultList.Add(newBus);
+                }
+                DatabaseConnection.shared.CloseLastConnection();
+            }
+            catch (SqlException exception)
+            {
+                Console.WriteLine(exception.Message);
+                Console.WriteLine("BusList is Empty");
+            }
+            return resultList;
+        }
+
+        public static void Put(string request)
+        {
+            DatabaseConnection.shared.Put(request);
+        }
+    }
+
+    public class Bus
+    {
+        // Id in a company database
+        public int id;
+        public string brand;
+        // Id in a country database
+        public int subId;
+        public double totalDistanceDriven;
+        public DateTime releaseDate;
+
+        public Bus(int id, string brand, int subId, double totalDistanceDriven, DateTime releaseDate)
+        {
+            this.id = id;
+            this.brand = brand;
+            this.subId = subId;
+            this.totalDistanceDriven = totalDistanceDriven;
+            this.releaseDate = releaseDate;
+        }
+    }
+}
---
+++ net-subject/CourseWork-BusSchedule/Networking/Models/IntineraryProxy.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace CourseWork_BusSchedule.Networking.Models
+{
+    public class IntineraryProxy
+    {
+        public static List<Itinerary> Get(string request)
+        {
+            List<Itinerary> resultList = new List<Itinerary>();
+            try
+            {
+                SqlDataReader reader = DatabaseConnection.shared.Get(request);
+                while (reader.Read())
+                {
+                    int id = Convert.ToInt32(reader.GetValue(0));
+                    string itinerary = Convert.ToString(reader.GetValue(1));
+                    double routeLength = Convert.ToDouble(reader.GetValue(2));
+                    double averageTime = Convert.ToDouble(reader.GetValue(3));
+                    int routesNumber = Convert.ToInt32(reader.GetValue(4));
+
+                    Itinerary newItinerary = new Itinerary(id, itinerary, routeLength, averageTime, routesNumber);
+                    resultList.Add(newItinerary);
+                }
+                DatabaseConnection.shared.CloseLastConnection();
+            }
+            catch (SqlException exception)
+            {
+                Console.WriteLine(exception.Message);
+                Console.WriteLine("ItineraryList is Empty");
+            }
+            return resultList;
+        }
+
+        public static void Put(string request)
+        {
+            DatabaseConnection.shared.Put(request);
+        }
+    }
+
+    public class Itinerary
+    {
+        public int id;
+        public string itinerary;
+        public double routeLength;
+        public double averageTime;
+        public int routesNumber;
+
+        public Itinerary(int id, string itinerary, double routeLength, double averageTime, int routesNumber)
+        {
+            this.id = id;
+            this.itinerary = itinerary;
+            this.routeLength = routeLength;
+            this.averageTime = averageTime;
+            this.routesNumber = routesNumber;
+        }
+    }
+}
