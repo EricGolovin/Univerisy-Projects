86f49dcb68838a921ed8b47148323bafc4fab260
Erik Golovin <erik_golovin@epam.com>
2021-03-09 02:26:53 +0200

    Add First Java HW

--- java/src/Main.java
+++ java/src/Main.java
@@ -9,45 +9,51 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        // Lab 1.1
-//        int matrixSize;
-//        RandomMatrix randomMatrix;
-//        Scanner defaultScanner = new Scanner(System.in);
-//
-//        System.out.println("Enter matrix size: ");
-//        matrixSize = defaultScanner.nextInt();
-//
-//        randomMatrix = new RandomMatrix(matrixSize, true);
-//        System.out.println(randomMatrix);
-//
-//        randomMatrix.rotateBy(MatrixRotationDegree.ONE_ROTATION);
-//        System.out.println(randomMatrix);
-//
-//        randomMatrix.resetRotation();
-//        System.out.println(randomMatrix);
+//         Lab 1.1
+        System.out.println("Lab 1.1");
+        int matrixSize;
+        RandomMatrix randomMatrix;
+        Scanner defaultScanner = new Scanner(System.in);
 
-        // Lab 1.2.1
-//        FractionOperation operation = new FractionOperation(4);
-//        System.out.println(operation);
-//
-//        FractionOperation newOperation = null;
-//        try {
-//            newOperation = operation.convertCollection();
-//        } catch(Exception exception) {
-//            System.out.println(exception.getMessage());
-//        }
-//        if (newOperation != null) {  System.out.println(String.format("Converted \n%s", newOperation)); };
+        System.out.println("Enter matrix size: ");
+        matrixSize = defaultScanner.nextInt();
 
-        // Lab 1.2.2
-//        LineOperation operation = new LineOperation(5);
-//        System.out.println(operation);
-//
-//        System.out.println(operation.getIntersectingLines());
-//        System.out.println(operation.getParallelLines());
+        randomMatrix = new RandomMatrix(matrixSize, true);
+        System.out.println(randomMatrix);
 
-        // Lab 1.3
-//        System.out.println(duplicatesExistIn(getFirstLetters("Hello from all over the World!")));
-//        System.out.println(duplicatesExistIn(getFirstLetters("Hello from all over the horld!")));
+        randomMatrix.rotateBy(MatrixRotationDegree.ONE_ROTATION);
+        System.out.println(randomMatrix);
+
+        randomMatrix.resetRotation();
+        System.out.println(randomMatrix);
+
+//         Lab 1.2.1
+        System.out.println("Lab 1.2.1");
+        FractionOperation fractionOperation = new FractionOperation(4);
+        System.out.println(fractionOperation);
+
+        FractionOperation newOperation = null;
+        try {
+            newOperation = fractionOperation.convertCollection();
+        } catch(Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+        if (newOperation != null) {  System.out.println(String.format("Converted \n%s", newOperation)); };
+
+//         Lab 1.2.2
+        System.out.println("Lab 1.2.2");
+        LineOperation lineOperation = new LineOperation(5);
+        System.out.println(lineOperation);
+
+        System.out.println(lineOperation.getIntersectingLines());
+        System.out.println(lineOperation.getParallelLines());
+
+//         Lab 1.3
+        System.out.println("Lab 1.3");
+        System.out.println("Hello from all over the World!");
+        System.out.println(duplicatesExistIn(getFirstLetters("Hello from all over the World!")));
+        System.out.println("Hello from all over the horld!");
+        System.out.println(duplicatesExistIn(getFirstLetters("Hello from all over the horld!")));
     }
 
     static public Boolean duplicatesExistIn(String text) {
